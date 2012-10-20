0fa0bd08721996f188e8b1bb0cae8fcb0faea425
Alma Madsen <madsenat@gmail.com>
2012-10-20 00:19:29 -0600

    updated .gitignore, index.html and main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+var betaCreator = null;
+
+function init() {
+	var outp = document.getElementById('outp');
+	betaCreator = BetaCreator(document.getElementById('srcImg'), {
+		zoom: 'contain',
+		height: 400,
+		width: '100%'
+	});
+}
+
+function saveData() {
+	if (!betaCreator)
+		return;
+
+	if (window.localStorage)
+		localStorage.bcData = betaCreator.getData();
+	else
+		alert(betaCreator.getData());
+}
+
+function loadData() {
+	if (!betaCreator)
+		return;
+
+	if (window.localStorage && localStorage.bcData)
+		betaCreator.loadData(localStorage.bcData);
+	else
+		alert("No data stored locally. You must have a browser that supports local storage and save first.");
+}
